31986bed880ecf662d627ad5e8a7a6e6b35e1527
vzvu3k6k <vzvu3k6k@gmail.com>
2022-11-27 01:48:43 +0900

    use @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: { 
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+  },
 };
